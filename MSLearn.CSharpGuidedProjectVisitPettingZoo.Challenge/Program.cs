9b025d72eba8501f4ddb0bed47b2db97413e5046
jra <julien.gault.50@gmail.com>
2024-03-19 12:47:00 +0100

    AssignGroup method

--- MSLearn.CSharpGuidedProjectVisitPettingZoo.Challenge/Program.cs
+++ MSLearn.CSharpGuidedProjectVisitPettingZoo.Challenge/Program.cs
@@ -9,10 +9,12 @@ string[] pettingZoo =
 
 RandomizeAnimals();
 
-// var group = AssignGroup();
+var group = AssignGroup();
 Console.WriteLine("School A");
 // PrintGroup(group);
 
+
+// Randomizes the pettingZoo array by randomly switching each entries two by two
 void RandomizeAnimals()
 {
     Random random = new Random();
@@ -25,4 +27,21 @@ void RandomizeAnimals()
         pettingZoo[i] = pettingZoo[r];
         pettingZoo[r] = temp;
     }
+}
+
+string[,] AssignGroup(int groups = 6)
+{
+    string[,] result = new string[groups, pettingZoo.Length / groups];
+
+    int start = 0;
+
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
 }
